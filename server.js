48ade495cfca693f8faf63c6f39d743d86d48745
onnoo <sweyjw@gmail.com>
2018-08-04 21:01:01 +0900

    commit kingcat_0.1

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var app = express();
+var server = require('http').Server(app);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+if (app.get('env') === 'development') {
+	app.locals.pretty = true;
+}
+
+if (app.get('env') === 'development') {
+	app.locals.pretty = true;
+}
+
+var conn = mysql.createConnection({
+	host: "aacy9rmotsalwh.c30xg8pih1rj.us-east-2.rds.amazonaws.com",
+	user: "root",
+	password: "19940106",
+	database: 'game_data'
+});
+
+conn.connect(function(err) {
+	if (err) throw err;
+	console.log('DB Connected!');
+});
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.post('/game', function(req, res){
+	res.redirect('/game');
+});
+
+app.get('/game', function(req, res){
+	res.render('game');
+});
+
+
+server.listen(3000, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
